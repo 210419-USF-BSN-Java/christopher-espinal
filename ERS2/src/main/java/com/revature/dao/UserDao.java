ec5eb8af2a3d8cfc229a85eb03fcf02bdca1c3df
espinalc1 <christopher.espinal@gmail.com>
2021-05-24 15:09:36 -0400

    folder cleanup

--- ERS2/src/main/java/com/revature/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.revature.dao;
-
-import com.revature.models.User;
-
-public interface UserDao extends GenericDao<User> {
-	public User getByEmail(String email);
-
-	public User getByUsername(String username);
-}
